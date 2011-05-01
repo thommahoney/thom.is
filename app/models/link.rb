0228c3568b8d32c1f1c3072deb57650193ba14ff
Thom Mahoney <thom@thelittleappfactory.com>
2011-05-01 00:33:31 -0700

    Cleanup around Links views, added some comments for the random string generation method.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,13 +1,19 @@
 require 'digest/sha1'
 class Link < ActiveRecord::Base
+  default_scope :order => 'created_at DESC'
+
   validates :short, :presence => true,
                     :uniqueness => true
+
   validates :long, :presence => true,
                    :format => { :with => /^http(s)*:\/\/.*$/i }
-  
+
   def self.random_string(length)
-    o = [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten;  
-    random_str = (0...length).map{ o[rand(o.length)] }.join;
+    # we want all characters a-z and A-Z
+    characters = [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten;
+    # generate a string of these characters given the length
+    random_str = (0...length).map{ characters[rand(characters.length)] }.join;
+    # in the off event that the random string already exists, try again
     if Link.find_by_short(random_str)
       return List.random_string(length)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 ThomIs::Application.routes.draw do
-  resources :links
   resources :tasks
   resources :people
   resource :session
 
+  resources :links
 
   match '/login' => 'sessions#new', :as => 'login'
   match '/logout' => 'sessions#destroy', :as => 'logout'
   match '/auth' => 'sessions#credentials'
 
   match '*short' => "links#redirect"
+  
   root :to => 'home#index'
 end
